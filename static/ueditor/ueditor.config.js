aa10f279a4182b482607e464249f03b2d5372c75
ankexun <wqw@ankexun.com>
2017-09-07 16:26:35 +0800

    修改serverUrl配置错误
    修改serverUrl配置错误

--- static/ueditor/ueditor.config.js
+++ static/ueditor/ueditor.config.js
@@ -30,7 +30,8 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "/demo3/controller"
+	// 此处必须和routes.go里配置的router.Any("/ueditor/controller", controllers.Action)对应
+        , serverUrl: "/ueditor/controller"
 
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的重新定义
         , toolbars: [[
